d06a2bea1f83c5e7c696f5ed60f2e0a9f0ba73bd
Fabio14-10 <fabitoroa1110@gmail.com>
2025-02-18 14:28:06 -0500

    Update Factura.java

--- EjercicioFactura/src/ejerciciofactura/Factura.java
+++ EjercicioFactura/src/ejerciciofactura/Factura.java
@@ -1,4 +1,3 @@
-
 package ejerciciofactura;
 import java.util.Scanner;
 
@@ -28,17 +27,37 @@ public class Factura {
             double Total=PrecioUni*Cantidad;
             double Descuento=Total*0.13;
             double Pago=Total+Descuento;
-        
         Factura.calcularDatos(Cantidad, PrecioUni);
+        System.out.println("El producto es "+Producto);
+        System.out.println("La cantidad Comprada es "+Cantidad);
+        System.out.println("El precio unitario es de: "+PrecioUni);
+        System.out.println(NombreCliente+" "+"El total de la factura es "+Total);
+        System.out.println("El impuesto dado por ser Estudiante es de el 13%");
+        System.out.println(NombreCliente+" "+"El total de la factura con su respectivo impuesto es "+Pago);
+        return true;
+}
+        else {
+            double Total1=PrecioUni*Cantidad;
+            double Descuento1=Total1*0.05;
+            double Pago1=Total1+Descuento1;
+        Factura.calcularDatos(Cantidad, PrecioUni);
+        System.out.println("El producto es "+Producto);
+        System.out.println("La cantidad Comprada es "+Cantidad);
+        System.out.println("El precio unitario es de: "+PrecioUni);
+        System.out.println(NombreCliente+" "+"El total de la factura es "+Total1);
+        System.out.println("El impuesto dado por no ser Estudiante es de el 5%");
+        System.out.println(NombreCliente+" "+"El total de la factura con su respectivo impuesto es "+Pago1);
         return false;
+      }  
     }
     public static void calcularDatos (int datoCantidad, int datoPrecioUni) {
-
+        System.out.println("Cantidad del producto a llevar: " + datoCantidad);
+        System.out.println("Precio del producto: " + datoPrecioUni);
 }
 
-    }
     public void mortarResultados(){
         System.out.println();
     }
     
 }
+
